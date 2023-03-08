fc93f3f87b31c944e4e1ff42eb604466d020ed91
eliskafoxx <eliska.foxx@gmail.com>
2023-03-08 14:01:55 +0100

    kostra zapniSe

--- src/main/java/cz/czechitas/ukol3/Pocitac.java
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -29,4 +29,26 @@ public class Pocitac {
     public void setPevnyDisk(Disk pevnyDisk) {
         this.pevnyDisk = pevnyDisk;
     }
+
+    public boolean isjeZapnuty() {
+        return jeZapnuty;
+    }
+
+    public void zapniSe() {
+        if (jeZapnuty) {
+            System.err.println("Chyba: PC je jiz zapnuto!");
+        } else if (cpu == null) {
+            System.err.println("Chyba: PC nelze zapnout bez procesoru!");
+        } else if (ram == null) {
+            System.err.println("Chyba: PC nelze zapnout bez operacni pameti!");
+        } else if (pevnyDisk == null) {
+            System.err.println("Chyba: PC nelze zapnout bez HD!");
+        } else {
+            jeZapnuty = true;
+            System.out.println("PC se zapnulo.");
+        }
+    }
 }
+//    public void vypniSe();
+
+

975302a8cc3df1cf88464436afc9bc892f228697
eliskafoxx <eliska.foxx@gmail.com>
2023-03-11 12:39:44 +0100

    cast 2 - demonstrace metod

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -21,49 +21,58 @@ public class HlavniProgram {
         eliskyDisk.setKapacita(245_107_195_904L);
         eliskyDisk.setVyuziteMisto(232_109_182_098L);
 
-        /* vypise chybu "PC nelze zapnout bez procesoru
+        /* vypise chybu "PC nelze zapnout bez procesoru!":
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.zapniSe();
         */
 
         eliskyPocitac.setCpu(eliskyProcesor);
 
-        /* vypise chybu "PC nelze zapnout bez operacni pameti!"
+        /* vypise chybu "PC nelze zapnout bez operacni pameti!":
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.zapniSe();
         */
 
         eliskyPocitac.setRam(eliskyPamet);
 
-        /* vypise chybu "PC nelze zapnout bez HD!"
+        /* vypise chybu "PC nelze zapnout bez HD!":
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.zapniSe();
         */
         eliskyPocitac.setPevnyDisk(eliskyDisk);
 
-        // dojde k zapnuti PC
+        // dojde k zapnuti PC:
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.zapniSe();
 
-        /* vypise chybu "PC je jiz zapnuto!"
+        /* vypise chybu "PC je jiz zapnuto!":
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.zapniSe();
          */
 
-        // vypnuti PC -> off
+        // vypnuti PC -> off:
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.vypniSe();
 
-        // nevypise chybu -> nic se nedeje
+        // nevypise chybu -> nic se nedeje:
         System.out.println(eliskyPocitac.toString());
         eliskyPocitac.vypniSe();
 
         // CAST 2
+        // nelze vytvaret soubory pri vypnutem PC - chyba (nic se nedeje):
+        eliskyPocitac.vytvorSouborOVelikosti(300_000_001L);
+
         // vytvareni a mazani souboru:
         eliskyPocitac.zapniSe();
-        eliskyPocitac.vytvorSouborOVelikosti(300_000_001);
-        eliskyPocitac.vymazSouborOVelikosti(300_000_001);
+        eliskyPocitac.vytvorSouborOVelikosti(300_000_001L);
+        eliskyPocitac.vymazSouborOVelikosti(300_000_001L);
+
+        // vypise chybu: "Nelze vytvorit pozadovany soubor, HD nema dostatecnou kapacitu!":
+        eliskyPocitac.vytvorSouborOVelikosti(999_999_000_001L);
+
+        // nelze mazat soubory pri vypnutem PC - chyba (nic se nedeje):
         eliskyPocitac.vypniSe();
+        eliskyPocitac.vymazSouborOVelikosti(300_000_001L);
     }
 
 }
